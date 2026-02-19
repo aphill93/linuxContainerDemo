71c0ff8dc732293856303b51681b4c33c086ffb2
poggioli32 <eric.reffett@emerson.com>
2026-02-19 15:05:56 -0600

    Update runlabview.sh
    added a draft change that just adds a comment.

--- runlabview.sh
+++ runlabview.sh
@@ -4,6 +4,8 @@ REPORT_PATH='/usr/local/natinst/ContainerExamples/Results.txt'
 MASSCOMPILE_DIR='/workspace/Test-VIs'
 LABVIEW_PATH='/usr/local/natinst/LabVIEW-2025-64/labviewprofull'
 
+# adding a comment  
+
 # Verify that the configuration file exists.
 if [ ! -f "$CONFIG_FILE" ]; then
   echo "Error: Configuration file not found at $CONFIG_FILE, exiting...!"
